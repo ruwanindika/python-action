a5106472809f23fde6f958e516341b942b5ff7b3
Ruwan <34013074+ruwanindika@users.noreply.github.com>
2024-03-21 14:55:18 +1100

    Update my-script.py

--- my-script.py
+++ my-script.py
@@ -1,5 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
 def main():
-  print(f"---->>>>>> Hello CI/CD")
+    item_list = []
+
+    driver=webdriver.Chrome()
+    driver.get('https://devops1.com.au/')
+    driver.maximize_window()
+
+    
+    service_list = driver.find_element(By.CSS_SELECTOR,'.dropdown-menu.show')
+    
+    for i in (service_list.text).splitlines():
+        print(i)
 
 if __name__ == "__main__":
   main()
