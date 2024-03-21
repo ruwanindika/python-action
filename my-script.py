81eaa922705094db5589d11128b3b424f53b3fcc
Ruwan <34013074+ruwanindika@users.noreply.github.com>
2024-03-21 15:15:08 +1100

    Update my-script.py

--- my-script.py
+++ my-script.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
 
 def main():
     item_list = []
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
 
-    driver=webdriver.Chrome()
+    driver=webdriver.Chrome(options=options)
     driver.get('https://devops1.com.au/')
-    driver.maximize_window()
+    # driver.maximize_window()
 
     
     service_list = driver.find_element(By.CSS_SELECTOR,'.dropdown-menu.show')
